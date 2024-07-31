4b6944e607435c41236aaa5dc4b8895171d853fd
Jonathan Brown <jbrown@acuity.network>
2024-07-31 11:04:24 +0700

    Add contract stub.

---
+++ test/AtomicSwap.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import {Test, console} from "forge-std/Test.sol";
+import {AtomicSwap} from "../src/AtomicSwap.sol";
+
+contract AtomicSwapTest is Test {
+    AtomicSwap public atomicSwap;
+
+    function setUp() public {
+        atomicSwap = new AtomicSwap();
+    }
+
+    function test_Increment() public {
+        // counter.increment();
+        // assertEq(counter.number(), 1);
+    }
+
+    function testFuzz_SetNumber(uint256 x) public {
+        // counter.setNumber(x);
+        // assertEq(counter.number(), x);
+    }
+}
